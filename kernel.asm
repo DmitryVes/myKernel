5fb8da46c3816a739077c904579d64170b4ece16
DmitryVes <94383632+DmitryVes@users.noreply.github.com>
2022-12-28 23:33:24 +0300

    Add files via upload

---
+++ kernel.asm
@@ -0,0 +1,96 @@
+section ._start
+bits 16
+global _start
+_start:
+    mov ax, 0x2401
+    int 0x15 ; enable A20 bit
+
+    mov ax, 0x3
+    int 0x10 ; set vga text mode 3
+
+    mov [disk], dl
+
+    mov ah, 0x2  ;read sectors
+    mov al, 6  ;sectors to read
+    mov ch, 0  ;cylinder idx
+    mov dh, 0  ;head disk
+    mov cl, 2  ;sector idx
+    mov dl, [disk]  ;disk idx
+    mov bx, copy_target  ;target_pointer
+    int 0x13
+    cli
+    lgdt [gdt_pointer] ; load the gdt table
+    mov eax, cr0 
+    or eax, 0x1 ; set the protected mode bit on special CPU reg cr0
+    mov cr0, eax
+    jmp CODE_SEG : boot2 ; long jump to the code segment
+
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+gdt_data:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+gdt_end:    
+gdt_pointer:
+    dw gdt_end - gdt_start
+    dd gdt_start
+
+disk:
+    db 0x0
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
+copy_target:
+
+bits 32
+
+global load_idt
+global kbd_handler
+global kernel
+
+extern kmain
+extern print_handler
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti 
+	ret
+
+kbd_handler:  
+	call print_handler
+	iretd
+
+boot2:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+kernel:
+    mov esp, kernel_stack_top
+    call kmain
+    hlt
+
+section .bss
+align 4
+kernel_stack_bottom: equ $
+    resb 16384 
+kernel_stack_top:
---
+++ master.cpp
@@ -0,0 +1,241 @@
+﻿#include "common.h"
+#include "kbdmap.h"
+
+#define IDT_SIZE 256
+
+static short* vga = (short*)0xb8000;
+static int screen_x = 0;
+static int screen_y = 0;
+
+extern "C" void kbd_handler(void);
+extern "C" void load_idt(unsigned long* idt_ptr);
+
+extern "C" void scroll_screen()
+{
+    for (int i = 0; i < 24 * 80; ++i)
+    {
+        vga[i] = vga[i + 80];
+    }
+    for (int i = 24 * 80; i < 25 * 80; ++i)
+    {
+        vga[i] = ' ';
+    }
+}
+
+extern "C" void move_cursor()
+{
+    u16int cursorLocation = screen_y * 80 + screen_x;
+    outb(0x3D4, 14);
+    outb(0x3D5, cursorLocation >> 8);
+    outb(0x3D4, 15);
+    outb(0x3D5, cursorLocation);
+}
+
+extern "C" void print_str(const char* s)
+{
+    while (*s != 0)
+    {
+        vga[screen_y * 80 + screen_x++] = 0x0700 | *s++;
+        move_cursor();
+    }
+}
+
+extern "C" void print_handler()
+{
+    outb(0x20, 0x20);
+
+    if ((inb(0x64) & 0x01) != 0x01)
+    {
+        return;
+    }
+
+    short scan_code = inb(0x60);
+
+    if (scan_code > 89)
+    {
+        return;
+    }
+
+    char kbd_char = kbd_US[scan_code];
+
+    if (kbd_char == '\n')
+    {
+        screen_x = 0;
+        screen_y++;
+    }
+    else if (kbd_char == '\b')
+    {
+        screen_x--;
+        vga[screen_y * 80 + screen_x] = ' ';
+    }
+    else if (kbd_char == '\t')
+    {
+        screen_x <<= 1;
+    }
+    else if (kbd_char != 0)
+    {
+        vga[screen_y * 80 + screen_x] = 0x0700 | kbd_char;
+        screen_x++;
+        if (screen_y >= 25)
+        {
+            scroll_screen();
+            screen_x = 0;
+            screen_y = 24;
+        }
+    }
+    if (screen_x >= 80)
+    {
+        screen_x = 0;
+        screen_y++;
+    }
+    move_cursor();
+}
+
+extern "C" void memset(unsigned long* dest, int ch, unsigned long cnt)
+{
+    volatile unsigned long* start = dest;
+    for (unsigned long i = 0; i < cnt; ++i)
+    {
+        start[i] = ch;
+    }
+}
+
+struct IDT
+{
+    unsigned short offset_lowerbits;
+    unsigned short offset_higherbits;
+    unsigned short selector;
+    unsigned char zero;
+    unsigned char type_attributes;
+};
+
+struct IDT IDT_deskriptors[IDT_SIZE];
+
+extern "C" void IDT_init()
+{
+    unsigned long idt_ptr[2];
+    unsigned long idt_address = (unsigned long)IDT_deskriptors;
+    idt_ptr[0] = (sizeof(struct IDT) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+    idt_ptr[1] = idt_address >> 16;
+
+    //memset(&idt_address, 0, sizeof(IDT_deskriptors) * IDT_SIZE);
+
+    unsigned long kbd_address = (unsigned long)kbd_handler;
+    IDT_deskriptors[0x21].offset_lowerbits = (kbd_address & 0xffff);
+    IDT_deskriptors[0x21].offset_higherbits = (kbd_address & 0xffff0000) >> 16;
+    IDT_deskriptors[0x21].zero = 0;
+    IDT_deskriptors[0x21].selector = 0x08;
+    IDT_deskriptors[0x21].type_attributes = 0x8e;
+
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+
+    outb(0x21, 0x04); 
+    outb(0xA1, 0x02); 
+
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+
+
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+
+    load_idt(idt_ptr);
+}
+
+extern "C" void kbd_init()
+{
+    outb(0x21, 0xFD); //enable kbd on IRQ1
+}
+
+extern "C" void outb(u16int port, u8int value)
+{
+    asm volatile("outb %1, %0" : : "dN" (port), "a" (value));
+}
+
+extern "C" u8int inb(u16int port)
+{
+    u8int ret;
+    asm volatile("inb %1, %0" : "=a" (ret) : "dN" (port));
+    return ret;
+}
+
+extern "C" u16int inw(u16int port)
+{
+    u16int ret;
+    asm volatile("inw %1, %0" : "=a" (ret) : "dN" (port));
+    return ret;
+}
+
+extern "C" int strlen(const char* str)
+{
+    int length = 0;
+    while (str[length] != '\0')
+    {
+        ++length;
+    }
+    return length;
+}
+
+extern "C" void itos(int n)
+{
+    while (n != 0)
+    {
+        vga[screen_y * 80 + screen_x] = 0x0700 | char(n % 10 + 48);
+        screen_x++;
+        move_cursor();
+        n /= 10;
+    }
+}
+
+extern "C" void turn_to_hex(u32int dec_n)
+{
+    int hex_GCD = 16777216; //k = 16776960;
+    while (dec_n > 0)
+    {
+        switch (dec_n / hex_GCD)
+        {
+        case 10: print_str("A");
+            break;
+        case 11: print_str("B");
+            break;
+        case 12: print_str("C");
+            break;
+        case 13: print_str("D");
+            break;
+        case 14: print_str("E");
+            break;
+        case 15: print_str("F");
+            break;
+        default:
+            itos(dec_n / hex_GCD);
+            break;
+        }
+        dec_n %= hex_GCD;
+        hex_GCD /= 16;
+    }
+}
+
+extern "C" void clear_screen()
+{
+    unsigned int j = 0;
+    while (j < 80 * 25 * 2)
+    {
+        vga[j] = ' ';
+        vga[j + 1] = 0x07;
+        j = j + 2;
+    }
+}
+
+extern "C" void kmain(void)
+{
+    IDT_init();
+    kbd_init();
+    while (1)
+    {
+        asm("hlt");
+    };
+}
